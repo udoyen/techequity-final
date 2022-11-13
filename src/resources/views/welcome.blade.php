166245918536d87aceca95a9f79d2db02bba95b2
udoyen <datameshprojects@gmail.com>
2022-11-13 10:12:29 +0100

    Adds database check

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -125,6 +125,18 @@
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
+                    <div>
+                        <strong>Database Connected: </strong>
+                        <?php
+                            try {
+                                \DB::connection()->getPDO();
+                                echo \DB::connection()->getDatabaseName();
+                                } catch (\Exception $e) {
+                                echo 'None';
+                            }
+                        ?>
+                    </div>
+
                 </div>
             </div>
         </div>
